b86f1c228e8407f30d2d17f76ded0c07a4e2c18c
Marc Mora Mendez <marc.mora.m@gmail.com>
2019-11-04 00:29:04 +0100

    testing interrupciones
    Ejercicio PEC4 de la actividad 1 Parte 3.
    Se introducen las interrupciones.

--- PEC4/Activitat1/Parte3.ino
+++ PEC4/Activitat1/Parte3.ino
@@ -1,1 +1,48 @@
-Fichero arduino de la actividad 1 parte 3.
+const int led = LED_BUILTIN;
+const int pinSalida = 7;
+const int pinLectura = 0;
+const long intervaloTiempo = 1000;
+volatile byte state = LOW;
+
+int estadoPin = LOW;
+unsigned long tiempoPrevio = 0;
+unsigned long tiempoActual = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(pinSalida, OUTPUT);
+  pinMode(pinLectura, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinLectura), escribeLed, CHANGE);
+}
+
+void loop() {
+  tiempoActual = millis();
+  procesaTiempos();
+  digitalWrite(led, state);
+}
+
+void procesaTiempos(){
+  if (tiempoActual - tiempoPrevio >= intervaloTiempo) {
+    tiempoPrevio = tiempoActual;
+    cambiaEstado();
+    escribePin();
+  }
+}
+
+void cambiaEstado(){
+  if (estadoPin == LOW) {
+      estadoPin = HIGH;
+    } else {
+      estadoPin = LOW;
+    }
+}
+
+void escribePin(){
+  digitalWrite(pinSalida, estadoPin);
+}
+
+void escribeLed(){
+  state = !state;
+
+}
