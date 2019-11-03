f253d0d9e4b68b0f6780a07c35872d1eea65a42e
Marc Mora Mendez <marc.mora.m@gmail.com>
2019-11-04 00:27:37 +0100

    Blink sin delay()
    Fichero del ejercicio PEC4 actividad 1 Parte 2
    Se utiliza la funcion millis();

--- PEC4/Activitat1/Parte2.ino
+++ PEC4/Activitat1/Parte2.ino
@@ -1,2 +1,36 @@
-Fichero arduino de la actividad 1 parte 2.
+const int led =  LED_BUILTIN;
+const long intervaloTiempo = 1000;
+
+int estadoLed = LOW;
+unsigned long tiempoPrevio = 0;
+unsigned long tiempoActual = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  tiempoActual = millis();
+  procesaTiempos();
+}
+
+void procesaTiempos(){
+  if (tiempoActual - tiempoPrevio >= intervaloTiempo) {
+    tiempoPrevio = tiempoActual;
+    cambiaEstado();
+    escribePin();
+  }
+}
+
+void cambiaEstado(){
+  if (estadoLed == LOW) {
+      estadoLed = HIGH;
+    } else {
+      estadoLed = LOW;
+    }
+}
+
+void escribePin(){
+  digitalWrite(led, estadoLed);
+}
 
