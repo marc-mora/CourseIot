1e7a70eef46b8f4b241e008a30b17b094e5ab388
Marc Mora Mendez <marc.mora.m@gmail.com>
2019-11-04 01:40:37 +0100

    cambios programa

--- PEC4/Activitat2/Parte1.ino
+++ PEC4/Activitat2/Parte1.ino
@@ -1,1 +1,21 @@
-Fichero arduino de la actividad 2 parte 1.
+#include "OTA.h"
+#include "SerialMarcadores.h"
+
+const int led =  LED_BUILTIN;
+boolean estadoLed = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  setup_OTA();
+}
+
+void loop() {
+  revisaOTA();
+  buscaMarcadores();
+  enciendeLed();
+}
+
+void enciendeLed(){
+    digitalWrite(led, estadoLed);
+}
