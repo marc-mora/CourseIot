15d55ffe89a1aaa896129176d7c692d3497ca86d
Marc Mora Mendez <marc.mora.m@gmail.com>
2019-12-09 19:50:46 +0100

    Update arduino_secrets.h

--- PEC8/pec8ArdMqtt/arduino_secrets.h
+++ PEC8/pec8ArdMqtt/arduino_secrets.h
@@ -1,7 +1,7 @@
 
-#define SECRET_SSID "KiwiHome"
-#define SECRET_PASS "Kiwi618938899"
-#define MQTT_CLIENT_ID "hostid"
-#define MQTT_HOST "54.154.59.72"
-#define MQTT_USER "grupo4"
-#define MQTT_PASS "Curso.Iot"
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define MQTT_CLIENT_ID ""
+#define MQTT_HOST ""
+#define MQTT_USER ""
+#define MQTT_PASS ""
